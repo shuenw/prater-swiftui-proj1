e26afe305b54cdf5ce9d88139faab29d90c4aba9
Dexter <shuenwang@gmail.com>
2020-12-11 23:57:24 -0600

    Initial Commit

---
+++ prater-swiftui-proj1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prater-swiftui-proj1
+//
+//  Created by Dexter Wang on 12/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
